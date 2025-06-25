ab5389820eef96e9c3e9c5918c75678f425bfad0
Rudra Pratap <149554363+rpratap2111@users.noreply.github.com>
2025-06-25 11:16:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: 'Internal server error' });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
